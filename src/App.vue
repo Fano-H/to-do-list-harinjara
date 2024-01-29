5e53158f79913087cc116e87d278104631c97318
fanomezana livenexx <r.fanomezana@livenexx.net>
2024-01-29 17:00:45 +0300

    number of not completed task design

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <nav class="navbar  navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
       <div class="container">
         <span class="navbar-brand">To Do Harinjara</span>
+        <div class="text-white">
+          <div class="alert alert-warning mb-0 ms-3" role="alert">
+            Number of not completed tasks : <span class="badge text-bg-danger fs-6">2</span>
+          </div>
+        </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
